a32f801f61b86e49915539a27c1450851adb39f8
Stuart Lee <lee.s@wehi.edu.au>
2016-03-21 17:56:00 +1100

    package skeleton

--- R/loadStructure.R
+++ R/loadStructure.R
@@ -1,6 +1,21 @@
 #' Read Structure Output
-#' @param "BLAH"
+#' @param  path directory containing output of STRUCTURE runs
+#' @param  n_runs number of runs for each K value
+#' @param logfile include STRUCTURE logs for each run (required if wanting to do MCMC plots)
 #' @export
-readStructure <- function(x){
-  return(x)
+readStructure <- function(path, n_runs, logfile = FALSE) {
+  # i/o checks
+  if(!is.character(path)) {
+    stop("Path must be character variable.")
+  }
+  if(!dir.exists(path)) {
+    stop("Path does not exist.")
+  }
+
+  if(!is.finite(n_runs) && !is.integer(n_runs)) {
+    stop("n_runs must be a finite integer")
+  }
+
+  # do the work
+
 }
--- R/starmie.R
+++ R/starmie.R
@@ -1,2 +1,21 @@
 # starmie.R
 # Place documentation for test datasets here.
+
+#' Constructor for starmie object
+#'
+#'@description starmie object for storing STRUCTURE/ADMIXTURE run information
+#'@details The \code{starmie} object is a list with 6 elements:
+#'  n_samples: number of samples in marker set
+#'  n_markers: number of SNPs/microsatellites in marker set
+#'  ploidy: ploidy of samples
+#'  sample_data: data.frame of sample meta information
+#'  structure_run: data.frame of STRUCTURE results
+#'  adxmiture_run: data.frame of ADMIXTURE results
+#'  clumpp: data.frame of CLUMPP results
+#'@return a \code{starmie} object
+#'@export
+starmie <- function() {
+ structure(list(n_samples = integer(), n_markers = integer(), ploidy = integer(),
+                sample_data = NULL, structure_run = NULL, admixture_run = NULL, clumpp = NULL),
+                class = "starmie")
+}
