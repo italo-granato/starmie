18a3b4e0c6e9f81c143f5b22a06cd9f14425a288
Stuart Lee <lee.s@wehi.edu.au>
2016-04-01 16:54:42 +1100

    test log files

--- R/starmie-methods.R
+++ R/starmie-methods.R
@@ -12,7 +12,32 @@ print.starmie <- function(starmie_obj) {
   cat(statement)
 }
 
-# ggplot method not sure if have to use ggproto here, look into this
-autoplot.starmie <- function() {
+#' ggplot method for starmie object
+#' @param starmie_obj an object of class \code{\link{starmie}}
+#' @param method a character corresponding to either 'admixture' or 'structure'
+#' @param type a character corresponding to type of plot to make (default 'all')
+#' @importFrom ggplot2 autoplot
+autoplot.starmie <- function(starmie_obj, method, type = "all", ...) {
+
+  starmie_theme <- theme_bw() + theme(panel.grid=element_blank(),
+                         panel.border=element_blank(),
+                         panel.grid.minor=element_blank(),
+                         panel.grid.major=element_blank(),
+                         axis.text.x=element_blank(),
+                         axis.ticks.x=element_blank(),
+                         legend.position="none")
+
+  # probably best to do case, switch here
+  if (method == "admixture") {
+
+  }
 
 }
+
+#' Return a tidy data frame from a starmie object for custom plotting
+#'
+#' @importFrom ggplot2 fortify
+fortify.starmie <- function(starmie_obj, method) {
+
+}
+
