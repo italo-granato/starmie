483a71485b31c1a82e5667fd5f6f57452b2d1265
Gerry Tonkin-Hill <g.tonkinhill@gmail.com>
2016-04-10 16:16:31 +1000

    some debugging

--- R/loadStructure.R
+++ R/loadStructure.R
@@ -9,7 +9,8 @@
 #' @import stringr
 #' @importFrom purrr map
 #' @export
-readStructure <- function(starmie_obj, filename, logfile = NULL) {
+#'
+loadStructure <- function(starmie_obj, filename, logfile = NULL) {
   # i/o checks
   if (!inherits(starmie_obj, "starmie")) stop("Not a valid starmie object.")
   if(!is.character(filename)) {
@@ -45,7 +46,7 @@ readStructure <- function(starmie_obj, filename, logfile = NULL) {
   alle_lines <- str_trim(alle_lines)
   alle_lines <- str_split_fixed(alle_lines, "\\s+", n=pops+1)
   alle_freqs <- alle_lines[,2:ncol(alle_lines)]
-  class(alle_freqs) <- "numeric"
+  suppressWarnings(class(alle_freqs) <- "numeric")
 
   #Average distances
   avg_dist_lines <- s_f[(which(grepl("^Average distances.*", s_f))+1):(which(grepl("^Estimated Ln.*", s_f))-2)]
@@ -73,10 +74,10 @@ readStructure <- function(starmie_obj, filename, logfile = NULL) {
   #Inferred ancestory individuals
   ances_lines <- s_f[(which(grepl("^Inferred ancestry of.*", s_f))+1):(which(grepl("^Estimated Allele Frequencies .*", s_f))-1)]
   ances_lines <- str_trim(ances_lines)
-  ances_lines <- str_split_fixed(ances_lines, "[\\s:]+", n=6)
+  ances_lines <- gsub("[(:)]", "", ances_lines)
+  ances_lines <- str_split_fixed(ances_lines, "\\s+", n=4+pops)
   header <- ances_lines[1,][1:3]
   ances_lines <- ances_lines[2:nrow(ances_lines),2:ncol(ances_lines)]
-  ances_lines[,2] <- str_replace_all(ances_lines[,2], "[()]", "")
   class(ances_lines) <- "numeric"
   ancest_df <- data.frame(ances_lines)
   colnames(ancest_df)[1:3] <- header
@@ -84,12 +85,13 @@ readStructure <- function(starmie_obj, filename, logfile = NULL) {
   #Cluster allele frequencies
   clust_allel_lines <- s_f[(which(grepl("^First column gives.*", s_f))+1):(which(grepl("^Values of parameters used.*", s_f))-1)]
   pos <- which(grepl("^Locus .*", clust_allel_lines))
+  clust_allel_lines <- gsub("[()%]", "", clust_allel_lines)
   clust_allel_lines <- unname(split(clust_allel_lines, cumsum(seq_along(clust_allel_lines) %in% pos)))
   clust_allele_list <- purrr::map(clust_allel_lines, function(x){
-    list(Locus=str_split(x[[1]], "\\s+")[[1]][2]
-         , AlleleNumber=str_split(x[[2]], "\\s+")[[1]][1]
-         , MissingDataPercentage=str_split(x[[3]], "\\s+")[[1]][1]
-         , FreqMatrix=str_split_fixed(str_trim(x[4:length(x)]), "\\s+", n=4)
+    list(Locus=as.numeric(str_split(x[[1]], "\\s+")[[1]][2])
+         , AlleleNumber=as.numeric(str_split(x[[2]], "\\s+")[[1]][1])
+         , MissingDataPercentage=as.numeric(str_split(x[[3]], "\\s+")[[1]][1])
+         , FreqMatrix=apply(str_split_fixed(str_trim(x[4:length(x)]), "\\s+", n=pops+2), 2, as.numeric)
     )
   })
 
@@ -130,7 +132,7 @@ readStructure <- function(starmie_obj, filename, logfile = NULL) {
     burn_lines <- burn_lines[!grepl("Rep#:   Lambda   Alpha.*", burn_lines)]
     burn_lines <- str_split_fixed(burn_lines, "\\s+", n=pops*2+4)
     burn_lines[,1] <- gsub(":","",burn_lines[,1])
-    burn_df <- data.matrix(data.frame(burn_lines, stringsAsFactors = FALSE))
+    suppressWarnings(burn_df <- data.matrix(data.frame(burn_lines, stringsAsFactors = FALSE)))
     colnames(burn_df) <- burn_header
 
     nonburn_header <- unlist(str_split(nonburn_lines[1], "\\s{2,}"))
@@ -140,8 +142,8 @@ readStructure <- function(starmie_obj, filename, logfile = NULL) {
     nonburn_df <- data.matrix(data.frame(nonburn_lines, stringsAsFactors = FALSE))
     colnames(nonburn_df) <- nonburn_header
 
-    starmie_obj$structure_run["burn_df"] = burn_df
-    starmie_obj$structure_run["nonburn_df"] = nonburn_df
+    starmie_obj$structure_run$burn_df = burn_df
+    starmie_obj$structure_run$nonburn_df = nonburn_df
   }
 
   starmie_obj
