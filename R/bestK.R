53604ccb0d6bbc8ee10c626209e5883b5667e0fb
Stuart Lee <lee.s@wehi.edu.au>
2016-07-19 18:43:19 +1000

    bestK now returns a tidy data frame with the results of computing the AIC,BIC,DIC if method = structure. closes #25

--- R/bestK.R
+++ R/bestK.R
@@ -4,11 +4,18 @@
 
 #' Determine a suitable K value from multiple Structure runs
 #' @param  runs a list of \code{\link{struct}} objects or \code{\link{admix}} objects.
-#' @param  method the method used to calculate the best K
+#' @param  method the method used to calculate the best K  one of c("evanno", "structure", "admixture")
 #' @param  make_plot whether of not to generate diagnostic plots
 #' @import dplyr
 #' @import ggplot2
 #' @export
+#' @return a data.frame containing  with columns containing the L_k,
+#' AIC, BIC, DIC and deltaK. If make_plot = TRUE a ggplot object is printed
+#' for the method of interest.
+#' @details By default bestK will produce diagnostic plots for the 'evanno' method,
+#' however if the K values are not ordered or there an even numbers of runs
+#' per K the 'structure' method will be implemented and delta K will not be
+#' returned in the output.
 #' @examples
 #' multi_K <- exampleStructure("multiple_runs")
 #' # Run the evanno method and display diagnostic plots.
@@ -25,22 +32,43 @@ bestK <- function(runs, method="evanno", make_plot=TRUE){
   #order structure runs and collect by K
   if ( all(run_types == "struct") ) {
     message("Creating diagnostic plots for structure runs.")
-    posterior_probs = data.frame(
-      K=unlist(lapply(runs, getK)),
-      pos_prob=unlist(lapply(runs, getPosterior)))
+    params_sizes <- lapply(runs, getD)
 
+    posterior_probs <- data.frame(K=unlist(lapply(runs, getK)),
+                                  pos_prob=unlist(lapply(runs, getPosterior)),
+                                  d = unlist(lapply(params_sizes, function(i) i$d)),
+                                  n = unlist(lapply(params_sizes, function(i) i$n)))
 
-    #If K are not sequential or do not have equal number of runs just
+    # fit stats
+    ll_summary <- matrix(unlist(lapply(runs, getFitStats)), ncol = 2, byrow = TRUE)
+    # compute average AIC/BIC over runs + standard errors
+    posterior_probs$aic <- -2*posterior_probs$pos_prob + 2*posterior_probs$d
+    posterior_probs$bic <- -2*posterior_probs$pos_prob + posterior_probs$d * log(posterior_probs$n)
+    # Gelman 2013 method for computing DIC
+    posterior_probs$dic <- -2*ll_summary[,1] + 2*ll_summary[,2]
+
+    # If K are not sequential or do not have equal number of runs just
     # perform the structure approach and produce a simpler plot
     Ks <- table(posterior_probs$K)
 
-    if ( !is.sequential(names(Ks)) | !all(Ks[[1]]==Ks) | method == "structure") {
+    if ( !is.sequential(names(Ks)) | !all(Ks[[1]]==Ks) | all(Ks == 1) | method == "structure") {
 
-      if (method!="structure") warning("WARNING! K values are not sequential or there are an uneven number of runs per K. Reverting to structure method instead.")
-      .bestK_structure(posterior_probs, make_plot)
+      if (method!="structure") {
+        if (all(Ks == 1)) {
+          stop("Not enough information to compute
+                  Evanno statistics, only single runs.")
+        }
+        warning("WARNING! K values are not sequential or there are
+                an uneven number of runs per K.
+                Reverting to structure method instead.")
+
+      }
+      model_ll <- .bestK_structure(posterior_probs, make_plot)
+      model_ll
 
     } else {
-      .bestK_evanno(posterior_probs, make_plot)
+      model_ll <- .bestK_evanno(posterior_probs, make_plot)
+      model_ll
     }
   } else if ( all(run_types == "admix") ) {
     message("Creating diagnositc plots for admixture runs")
@@ -50,26 +78,33 @@ bestK <- function(runs, method="evanno", make_plot=TRUE){
       stop("Need log file information to produce diagnositc plots")
     }
     .bestK_admixture(log_df, make_plot)
-
   }
 
 }
 
 .bestK_evanno <- function(posterior_probs, make_plot) {
   # plot delta K
-  #split by K and calculate the first and second derivatives
+  # split by K and calculate the first and second derivatives
 
   split_posterior_probs <- split(posterior_probs, posterior_probs$K)
   split_posterior_probs <- split_posterior_probs[order(as.numeric(names(split_posterior_probs)))]
   # empty data frame for storing results
   output_variables <- c("L(K)", "L'(K)",  "L''(K)", "delta K")
   output_values <- list()
-  for (k in 2:(length(split_posterior_probs)-1)) {
-    dK <- split_posterior_probs[[k]]$pos_prob - split_posterior_probs[[k-1]]$pos_prob
-    ddK <- split_posterior_probs[[k+1]]$pos_prob-2*split_posterior_probs[[k]]$pos_prob+split_posterior_probs[[k-1]]$pos_prob
+  k_all <- min(posterior_probs$K):max(posterior_probs$K)
+  for (k in k_all) {
     LK <-  split_posterior_probs[[k]]$pos_prob
 
-    if ( sd(LK) == 0 ) stop("No deviation between runs of the same K. Evanno statistics cannot be computed.")
+    if(k > min(k_all) & k < max(k_all)) {
+      dK <- split_posterior_probs[[k]]$pos_prob - split_posterior_probs[[k-1]]$pos_prob
+      ddK <- split_posterior_probs[[k+1]]$pos_prob-2*split_posterior_probs[[k]]$pos_prob+split_posterior_probs[[k-1]]$pos_prob
+      if ( isTRUE(sd(LK) == 0 ) )stop("No deviation between runs of the same K. Evanno statistics cannot be computed.")
+
+    } else {
+      dK <- rep(NA, length(LK))
+      ddK <- rep(NA, length(LK))
+    }
+
 
     output_values[[k]] <-  data.frame(K = as.numeric(names(split_posterior_probs)[k]),
                                                 variable = output_variables,
@@ -83,46 +118,45 @@ bestK <- function(runs, method="evanno", make_plot=TRUE){
   if (make_plot) {
     gg <- ggplot(posterior_probs_summary, aes(x=K, y=value)) +
       geom_point() +
-      geom_errorbar(aes(ymax=value+sd, ymin=value-sd)) +
       facet_wrap(~variable, ncol=2, scales = "free_y") +
+      geom_errorbar(aes(ymax=value+sd, ymin=value-sd)) +
       theme_bw() +
       scale_x_continuous(breaks = min(posterior_probs_summary$K):max(posterior_probs_summary$K))
     suppressWarnings(print(gg))
   }
 
-  best <- posterior_probs_summary[posterior_probs_summary$variable=="delta K",]
-  best <- best$K[which.max(best$value)]
-  message("Probs not the best K, though hey. lol.")
-  return(best)
+  return(posterior_probs_summary)
 
 }
 .bestK_structure <- function(posterior_probs, make_plot) {
   # look for change point in log likelihood plot
-  #summarise data by K
-  posterior_probs_summary <- group_by(posterior_probs, K) %>%
-    summarize(
-      runs = n(),
-      meanK = mean(pos_prob),
-      sdK = sd(pos_prob)
-    )
-
+  # summarise data by K
+  posterior_probs_byK <- split(posterior_probs, posterior_probs$K)
+  posterior_probs_summary <- do.call("rbind", lapply(posterior_probs_byK,
+                                                     function(i) data.frame(K = unique(i$K),
+                                                                            variable = c('L(K)', 'AIC', 'BIC', 'DIC'),
+                                                                            value = c(mean(i$pos_prob), mean(i$aic), mean(i$bic), mean(i$dic)),
+                                                                            sd = c(sd(i$pos_prob), sd(i$aic), sd(i$bic), sd(i$dic)))))
   #generate plot
   if (make_plot) {
-    gg <- ggplot(posterior_probs_summary, aes(x=K, y=meanK)) +
+    gg <- ggplot(posterior_probs_summary, aes(x=K, y=value)) +
       geom_point() +
-      geom_errorbar(aes(ymax=meanK+sdK, ymin=meanK-sdK), width=0.1) +
+      geom_errorbar(aes(ymax=value+sd,
+                        ymin=value-sd), width=0.1) +
       theme_bw() +
+      facet_wrap(~variable, ncol=2, scales = "free_y") +
       scale_x_continuous(breaks = min(posterior_probs_summary$K):max(posterior_probs_summary$K)) +
       ylab("Mean log posterior probability")
     suppressWarnings(print(gg))
   }
 
-  message("Probs not the best K, though hey. lol.")
-  return(posterior_probs_summary$K[which.max(posterior_probs_summary$meanK)])
+  return(posterior_probs_summary)
 
 }
+
+
+
 .bestK_admixture <- function(log_df, make_plot) {
-  message("Probs not the best K, though hey. lol.")
 
   if (make_plot) {
     gg <- ggplot(log_df, aes(x = K, y = CVerror)) + geom_point() + theme_bw()
@@ -136,3 +170,5 @@ is.sequential <- function(x){
   x <- as.numeric(x)
   all(diff(x) == diff(x)[1])
 }
+
+
