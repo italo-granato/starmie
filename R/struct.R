849389b1fca9500db912b7e7a658b9cadbc125ab
Stuart Lee <lee.s@wehi.edu.au>
2016-07-20 16:26:27 +1000

    implements structList addresses #20, general doc tidying

--- R/struct.R
+++ R/struct.R
@@ -3,20 +3,34 @@
 
 #' Constructor for struct object
 #' @description struct object for storing structure run information
-#' @details The \code{structure} object is a list with 11 elements:
+#' @details The \code{\link{struct}} object is a list with 11 elements:
+#'
 #'  K: number of clusters estimated by Structure
+#'
 #'  run_params: the run paramters given to the Structure program
+#'
 #'  mem_df: assigned cluster membership proportions
+#'
 #'  alle_freqs: Estimated allele frequencies
+#'
 #'  avg_dist_df: Average distance between individuals
+#'
 #'  fit_stats_df: Model fit statistics
+#'
 #'  fst_df: Fst values
+#'
 #'  ancest_df: Inferred ancestory of individuals
+#'
 #'  clust_allele_list: Cluster allele frequencies
+#'
 #'  burn_df: Burn in MCMC iteration output
+#'
 #'  nonburn_df: Main MCMC iteration output
-#' @return a \code{structure} object
+#' @return a \code{\link{struct}} object
 #' @export
+#' @seealso \code{\link{loadStructure}} for reading in STRUCTURE out_f files.
+#'
+#' \code{\link{structList}} for manipulating multiple struct objects
 struct <- function() {
   structure(list(K = NULL, run_params = NULL, mem_df = NULL,
                  alle_freqs = NULL, avg_dist_df = NULL,
@@ -24,6 +38,30 @@ struct <- function() {
                  clust_allele_list = NULL,
                  burn_df=NULL, nonburn_df=NULL),
             class = "struct")
+}
+
+#' Constructor for a structList object
+#' @description  the structList class is a conatainer for storing a collection
+#' of struct objects.
+#' @param ... a list of a \code{\link{struct}} objects
+structList <- function(...) {
+
+  listOut <- list(...)
+
+  if (length(listOut) == 1L && is.list(listOut[[1L]])) {
+    listOut <- listOut[[1L]]
+    if (inherits(listOut, "struct")) {
+      return(listOut)
+    }
+
+  }
+  if (length(listOut) == 0L) {
+    stop("a structList must contain at least one struct object")
+  } else {
+    if (!all(sapply(listOut, is, 'struct')))
+      stop("all elements in '...' must be struct objects")
+    structure(listOut, class = "structList")
+  }
 }
 
 #' Accessor methods for struct objects
@@ -95,9 +133,6 @@ exampleStructure <- function(example_type) {
                          pattern = ".*K3.*out_f",
                          full.names = TRUE)
 
-    #k3_log <- list.files(structure_files,
-    #                     pattern = ".*K3.*log",
-    #                     full.names = TRUE)
     lapply(k3_out, loadStructure)
   } else if (example_type == "mcmc_diagnostics") {
     k10_out <- list.files(structure_files,
@@ -106,7 +141,7 @@ exampleStructure <- function(example_type) {
     k10_log <- list.files(structure_files,
                          pattern = ".*K10.*log",
                          full.names = TRUE)
-    mapply(loadStructure, k10_out, k10_log, SIMPLIFY = FALSE)
+    structList(mapply(loadStructure, k10_out, k10_log, SIMPLIFY = FALSE))
   } else if (example_type == "barplot") {
     k6_out <- list.files(structure_files,
                          pattern = "^locprior_K6.out_f",
@@ -118,7 +153,7 @@ exampleStructure <- function(example_type) {
                         pattern = "out_f$",
                         full.names = TRUE)
     sequential <- runs[grepl("^run2|^locprior", basename(runs))]
-    lapply(sequential, loadStructure)
+    structList(lapply(sequential, loadStructure))
   }
 
 }
@@ -135,4 +170,13 @@ print.struct <- function(x, ...) {
   logfile <- !is.null(x$burn_df)
   cat(paste("\nMCMC diagnostics available:", logfile))
 
+}
+
+#' @export
+print.structList <- function(x, ...) {
+  n <- length(x)
+  cat(paste("structList object containing", n, " STRUCTURE runs.\n"))
+  cat(paste("Number of Ks by number of runs:"))
+  Ks <- table(unlist(lapply(x, getK)))
+  print(Ks)
 }
