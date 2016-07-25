9960290c54c418a96f6b0bbb1632d1c0150f51e8
Stuart Lee <lee.s@wehi.edu.au>
2016-07-25 17:18:14 +1000

    compute MDS between clusters

--- R/plotMDS.R
+++ R/plotMDS.R
@@ -2,9 +2,14 @@
 #
 
 #' Plot prinicipal compenents between inferred clusters
-#' @param structure_obj
+#' @param structure_obj a \code{\link{struct}} object
+#' @param method one of "nnd" or "jsd"
+#' @details "nnd" uses the nucleotide distance matrix estimated by STRUCTURE
+#' to construct the principal coordinates, sizing the points by the expected
+#' heterozygosity within a cluster. "jsd" produces a principal coordinates
+#' from the Jensen Shannon Divergence metric as used by the 'ldavis' package.
 #' @import ggplot2
-#'
+#' @importFrom  proxy dist
 plotMDS <- function(structure_obj, method = "nnd") {
   # gather visual elements
 
@@ -21,11 +26,45 @@ plotMDS <- function(structure_obj, method = "nnd") {
 
     mds_df <- data.frame(clust_eh, PC1 = mds_clust[,1], PC2 = mds_clust[,2])
 
-    ggplot(mds_df, aes(x = PC1, y = PC2, size = Avg.dist)) + geom_point() + theme_bw()
+    ggplot(mds_df, aes(x = PC1, y = PC2, size = Avg.dist)) +
+      geom_point() +
+      ggrepel::geom_text_repel(
+        aes(label = Cluster),
+        size = 4,
+        box.padding = unit(0.35, "lines"),
+        point.padding = unit(0.3, "lines")
+      ) +
+      guides(size = guide_legend(title = "Expected Heterozygosity"))+
+      theme_bw()
+
+
+  } else if (method == "jsd") {
+    Q <- getQ(structure_obj)
+    dist_xy <- proxy::dist(t(Q), method = .JSD)
+    mds_clust <- cmdscale(dist_xy)
+    mds_df <- data.frame(Cluster = as.integer(sub("Cluster ", "", colnames(Q))),
+                         Relative.Contribution = colSums(Q) / sum(Q),
+                         PC1 = mds_clust[,1],
+                         PC2 = mds_clust[,2])
+    ggplot(mds_df, aes(x = PC1, y = PC2, size = Nk)) +
+    geom_point() +
+      ggrepel::geom_text_repel(
+        aes(label = Cluster),
+        size = 4,
+        box.padding = unit(0.35, "lines"),
+        point.padding = unit(0.3, "lines")
+      ) +
+      theme_bw()
+
+
 
 
   } else {
-    return(NULL)
+    stop("Not a valid method, must be either 'nnd' or 'ldavis'")
   }
 
 }
+
+.JSD<- function(x,y) sqrt(0.5 * .KLD(x, (x+y)/2) + 0.5 * .KLD(y, (x+y)/2))
+.KLD <- function(x,y) sum(x * log(x/y))
+
